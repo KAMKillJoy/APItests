7716d0dd6eab70a4c7f5787e37d9549cc3ff8049
John Doe <kamkilljoy@gmail.com>
2024-10-14 10:15:19 +0400

    работаем

--- Tests/Test_create.py
+++ Tests/Test_create.py
@@ -1,10 +1,21 @@
 from API.api_client import ApiClient
-
 import json
+from models.create_model import EntityData
 
-# Открываем JSON-файл и загружаем его содержимое
+# Загрузка данных из JSON-файла
 with open('../Test_data/create.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
-
+# Создание экземпляра модели Pydantic с загруженными данными
+entity_data = EntityData(**data)
+# Сериализация данных в JSON
+json_data = entity_data.model_dump_json()
+# Подготовка kwargs с заголовками и данными
+kwargs = {
+    "headers": {
+        "Content-Type": "application/json"
+    },
+    "data": json_data  # Передача сериализованных данных
+}
 APIC = ApiClient()
-APIC.get("/api/create", data)
+resp = APIC.post("/api/create", **kwargs)
+print (resp.status_code)
